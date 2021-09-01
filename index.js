a8b54d195064cde4569573da564e3b2821998d5b
Yassine Rassy <47438695+yassineDevox@users.noreply.github.com>
2021-09-01 13:17:46 +0200

    string literal exemple

--- index.js
+++ index.js
@@ -2,17 +2,26 @@
 // const TAILLE = 40;
 // TAILLE=34;
 
-var a=1;
-var b=2;
+// var a=1;
+// var b=2;
 
-if(b==0) alert('ERROR DIVISION')
-else {
-    let d = a/b
-    alert("d= "+d)
-}
+// if(b==0) alert('ERROR DIVISION')
+// else {
+//     let d = a/b
+//     alert("d= "+d)
+// }
 // console.log(d) Error
 
-for(let i = 0; i < 6; i++){
-    console.log(i)
-}   
+// for(let i = 0; i < 6; i++){
+//     console.log(i)
+// }   
 // console.log(i) Error
+
+//----------- string literal
+let name = "John"
+// using normal quotes
+console.log("Hello "+name+", how are you ?");
+// using ES6 template literals
+console.log(`
+Hello ${name}, how are you ?
+`);
