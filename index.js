a6980dee9cca1ba3b93e398158667dbf762d5861
Yassine Rassy <47438695+yassineDevox@users.noreply.github.com>
2021-09-01 14:26:48 +0200

    array functions ES6  exemple

--- index.js
+++ index.js
@@ -41,18 +41,91 @@
 // JavaScript (ES5)
 // function multiply() { return 2 * 3; };
 // ES6
-const multiply= () => console.log(2 * 3) ;
+// const multiply= () => console.log(2 * 3) ;
 
-//appel
-multiply()
+// //appel
+// multiply()
 
-let sayHello = () => alert`Hello`;
-let sayBye = () => alert`Bye`;
-//sayHello(); // “Hello” will be alerted
-// Let’s create a function that takes an argument and call it as if it was a function
-let doSomething = param => {
-    param();
-};
-//  Now any function we send to “doSomething()” function will be called right away
-doSomething(sayHello); // “Hello” will be alerted
-doSomething(sayBye); // “Bye” will be alerted
+// let sayHello = () => alert`Hello`;
+// let sayBye = () => alert`Bye`;
+// //sayHello(); // “Hello” will be alerted
+// // Let’s create a function that takes an argument and call it as if it was a function
+// let doSomething = param => {
+//     param();
+// };
+// //  Now any function we send to “doSomething()” function will be called right away
+// doSomething(sayHello); // “Hello” will be alerted
+// doSomething(sayBye); // “Bye” will be alerted
+//------------- Array functions ES6
+const people = [{ name: 'Max' }, { name: 'Jack 😃' }, { name: 'Marry' }]
+// JavaScript
+// function findPerson(name) {
+//     for (let i = 0; i < people.length; i++) {
+//         let person = people[i]
+//         if (person.name == name) {
+//             return person
+//         }
+//     }
+// }
+// ES6
+function findPerson(name) {
+    return people.find(person => person.name == name)
+}
+
+
+// JavaScript
+// function showEachOne(name) {
+//     for (let i = 0; i < people.length; i++) {
+//         alert(people[i].name)
+//     }
+// }
+
+// ES6
+const showEachOne =
+    name => people.forEach(person => console.log(person.name));
+
+// JavaScript
+// function filterProducts() {
+//     let cheapProducts = [];
+//     for (let i = 0; i < products.length; i++) {
+//         if (products[i].price < 10) cheapProducts.push(products[i]);
+//     }
+//     return cheapProducts;
+// }
+
+// ES6
+let products = [{name:"Milk",price:15}, {name:"Water", price:9}, {name:"Bread", price:5}];
+
+const filterProducts = () => products.filter(product => product.name != "Milk");
+
+
+///------ extraire toute les nom des produits ayant prix<10
+function f1(){
+    let cheaperNameProduct = []
+    for (let i = 0; i < products.length; i++) {
+        const p = products[i];
+        if(p.price<10)
+        {
+            cheaperNameProduct.push(p.name)
+        }
+    }
+    return cheaperNameProduct;
+}
+
+//ES6
+let f1_ES6 = () => products
+                        .filter(p=>p.price<10)
+                        .map(p=>p.name+" dadzad")
+
+
+const data = [5, 10, 15, 20, 25]
+const res = data.reduce((total, currentValue) =>  total + currentValue)
+console.log(res)// 75
+
+console.log(findPerson("Jack"))
+showEachOne();
+// products = filterProducts()
+// console.table(products)
+// console.log(f1())
+console.log(f1_ES6())
+
