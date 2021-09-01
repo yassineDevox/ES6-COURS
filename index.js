1354bc9abaeeecb347880f074c636c6defdb7c08
Yassine Rassy <47438695+yassineDevox@users.noreply.github.com>
2021-09-01 12:55:05 +0200

    let vs var exemple

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-
-
-
 //const 
-const TAILLE = 40;
-TAILLE=34;
+// const TAILLE = 40;
+// TAILLE=34;
+
+var a=1;
+var b=2;
+
+if(b==0) alert('ERROR DIVISION')
+else {
+    let d = a/b
+    alert("d= "+d)
+}
+// console.log(d) Error
+
+for(let i = 0; i < 6; i++){
+    console.log(i)
+}   
+// console.log(i) Error
